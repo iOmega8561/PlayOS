846860992aec6260e3e1e343e3a03e3a5dec690d
iOmega8561 <giusepperocco38@gmail.com>
2025-02-09 17:07:29 +0100

    Re-ordered LOCs in Application.ViewFactory

--- PlayOS.swiftpm/Model/Application.ViewFactory.swift
+++ PlayOS.swiftpm/Model/Application.ViewFactory.swift
@@ -30,27 +30,25 @@ extension Application {
         static func make(for application: Application) -> Self {
             return .init(application.contentType)
         }
-        
-        private let _build: (any Model) -> AnyView
 
         let makeModel: () -> any Model
 
+        private let _build: (any Model) -> AnyView
+        
+        func build(appModel: any Model) -> AnyView {
+            return _build(appModel)
+        }
+        
         private init<Content: Application.Content>(_ contentType: Content.Type) {
 
             self.makeModel = { Content.AppModel() }
             
             _build = { appModel in
-                
                 guard let typedModel = appModel as? Content.AppModel else {
                     fatalError("Invalid appModel type provided for \(Content.self)")
                 }
-                
                 return AnyView(Content(appModel: typedModel))
             }
-        }
-        
-        func build(appModel: any Model) -> AnyView {
-            return _build(appModel)
         }
     }
 }
